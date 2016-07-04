aadf3cce2923bde0ad2a0271c64f90d73ccd65fd
rmuri005 <rmuri005@fiu.edu>
2016-07-03 20:34:41 -0400

    Assesment view and evaluation

---
+++ Code/webapp/app/Test/Case/Model/EvaluationTest.php
@@ -0,0 +1,40 @@
+<?php
+App::uses('Evaluation', 'Model');
+
+/**
+ * Evaluation Test Case
+ *
+ */
+class EvaluationTest extends CakeTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'app.evaluation'
+	);
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+		$this->Evaluation = ClassRegistry::init('Evaluation');
+	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		unset($this->Evaluation);
+
+		parent::tearDown();
+	}
+
+}
